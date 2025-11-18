10033e0167db7b1ae6346499d1f1e23ac86af701
Simonblue003 <therealgamerstg@gmail.com>
2025-11-18 11:40:50 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+// App.js
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from './src/screens/Home'
+import SessionPlayer from './src/screens/SessionPlayer'
+
+const Stack = createNativeStackNavigator()
+
+export default function App(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SessionPlayer" component={SessionPlayer} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
