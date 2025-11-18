c96399b0671691e1b62296550c656b5e11370ebb
Simonblue003 <therealgamerstg@gmail.com>
2025-11-18 12:27:40 +0300

    Create SessionsListScreen.js

---
+++ src/screens/SessionsListScreen.js
@@ -0,0 +1,11 @@
+// optional detailed list view - lightweight version
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function SessionsListScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text style={{ padding: 20 }}>Sessions list (optional screen)</Text>
+    </SafeAreaView>
+  );
+}
