479e0643b79ac50d44489baff5eda1b8a1b5b5ed
Simonblue003 <therealgamerstg@gmail.com>
2025-11-24 10:23:33 +0300

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,9 @@
+export default ({ config }) => {
+  return {
+    ...config,
+    extra: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    },
+  };
+};
