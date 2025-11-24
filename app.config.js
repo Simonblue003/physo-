662af05a143bfad9b25959e393834882842c97c1
Simonblue003 <therealgamerstg@gmail.com>
2025-11-24 13:20:18 +0300

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -7,3 +7,12 @@ export default ({ config }) => {
     },
   };
 };
+export default ({ config }) => {
+  return {
+    ...config,
+    extra: {
+      EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    },
+  };
+};
