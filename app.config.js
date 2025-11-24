a9c606ffd0f68d0bdff311cc3c06cf5226bfa61f
Simonblue003 <therealgamerstg@gmail.com>
2025-11-24 13:45:52 +0300

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,8 +1,21 @@
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    eas: {
-      projectId: 'ec60ae2e-cef0-40bb-8cd4-b51730bf5998'
+export default ({ config }) => {
+  return {
+    ...config,
+    expo: {
+      ...(config.expo || {}),
+      name: "Desk Reset",
+      slug: "desk-reset",
+      owner: "martiankings",
+      android: {
+        ...(config.expo?.android || {}),
+        package: "com.martiankings.deskreset"
+      },
+      extra: {
+        ...(config.expo?.extra || {}),
+        eas: {
+          projectId: "ec60ae2e-cef0-40bb-8cd4-b51730bf5998"
+        }
+      }
     }
-  }
-});
+  };
+};
